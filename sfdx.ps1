55b732bf1631d2e9d0512274faf087b7be39fc30
Suraj <85.suraj@gmail.com>
2020-03-15 17:01:16 -0400

    modify comments

--- sfdx.ps1
+++ sfdx.ps1
@@ -20,23 +20,23 @@ function aliases{sfdx force:alias:list}
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
-<# file for storing sfdx commands for faster lookup#>
+# file for storing sfdx commands for faster lookup
 $sfdxCommandsFilepath = "$HOME/commands.sfdx"
 
-<# The below script is executed in the background when a new ps session starts to pull all sfdx commands into a file#>
+# The below script is executed in the background when a new ps session starts to pull all sfdx commands into a file
 $sfdxCommandsFileCreateBlock = {
     param($filePath)
     sfdx commands | Out-File "$filePath"
 }
 
-<# executes the above script in the background so user is not waiting for the shell to start#>
+# executes the above script in the background so user is not waiting for the shell to start
 Start-Job -ScriptBlock $sfdxCommandsFileCreateBlock -ArgumentList "$sfdxCommandsFilepath" | Out-Null
 
-<# script block for autocomplete. looks up matching commands from the file created above#>
+# script block for autocomplete. looks up matching commands from the file created above
 $scriptBlock = {
     param($CommandName, $wordToComplete, $cursorPosition)
 
     @($(Get-Content -Path $sfdxCommandsFilepath)) -match "$("$wordToComplete".replace('sfdx ','')).*"
 }
-<# register the above script to fire when tab is pressed following the sfdx command#>
+# register the above script to fire when tab is pressed following the sfdx command
 Register-ArgumentCompleter -Native -CommandName sfdx -ScriptBlock $scriptBlock
