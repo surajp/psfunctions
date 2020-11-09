08104085f4fff825f6eb34b996aba474a18dbaed
Suraj <85.suraj@gmail.com>
2020-11-09 10:27:47 -0500

    Add createables function for querying createable fields

--- sfdx.ps1
+++ sfdx.ps1
@@ -19,24 +19,4 @@ function aliases{sfdx force:alias:list}
 [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11"
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-# file for storing sfdx commands for faster lookup
-$sfdxCommandsFilepath = "$HOME/commands.sfdx"
-
-# The below script is executed in the background when a new ps session starts to pull all sfdx commands into a file
-$sfdxCommandsFileCreateBlock = {
-    param($filePath)
-    sfdx commands | Out-File "$filePath"
-}
-
-# executes the above script in the background so user is not waiting for the shell to start
-Start-Job -ScriptBlock $sfdxCommandsFileCreateBlock -ArgumentList "$sfdxCommandsFilepath" | Out-Null
-
-# script block for autocomplete. looks up matching commands from the file created above
-$scriptBlock = {
-    param($CommandName, $wordToComplete, $cursorPosition)
-
-    @($(Get-Content -Path $sfdxCommandsFilepath)) -match "$("$wordToComplete".replace('sfdx ','')).*"
-}
-# register the above script to fire when tab is pressed following the sfdx command
-Register-ArgumentCompleter -Native -CommandName sfdx -ScriptBlock $scriptBlock
+function createables{ param ($s,$u) return sfdx force:schema:sobject:describe -s $s (IIf $u "-u$($u -Replace('\n',''))" "") --json | jq '.result.fields[] | select (.createable==true) | .name' }
